4643ae05aedaaa7ff5bb798afb517b4a0744de20
Himlay <contact@himalay.com.np>
2020-05-17 17:58:38 +0545

    Refactor KeyValue component
    - Fix: page-data.json net::ERR_ABORTED 404 error

--- src/components/KeyValue/KeyValue.tsx
+++ src/components/KeyValue/KeyValue.tsx
@@ -5,6 +5,106 @@ import Foldable from '@components/Foldable'
 import mediaqueries from '@styles/media'
 import Mark from './Mark'
 
+interface Props {
+  className?: string
+  objKey?: string
+  objValue?: string | number | Props[]
+  to?: string
+  isLast?: boolean
+}
+const concatString = (x: string | number = '', y: string | number) => `${x}${y}`
+
+const getValueElement = (to?: string, objValue: string | number = '', valueClassName = '') => {
+  if (to) {
+    const isExternal = /^(http|mail|tel)/.test(to)
+    if (isExternal || /\.xml$/.test(to)) {
+      return (
+        <a href={to} className={valueClassName} {...(isExternal ? { target: '_blank', rel: 'noreferrer' } : {})}>
+          {objValue}
+        </a>
+      )
+    } else {
+      return (
+        <Link to={to} className={valueClassName}>
+          {objValue}
+        </Link>
+      )
+    }
+  }
+
+  return <span className={valueClassName}>{objValue}</span>
+}
+const getValueClassName = (objValue?: string | number) => `value-${typeof objValue === 'number' ? 'number' : 'string'}`
+
+const KeyValue: React.FC<Props> = ({ className, children, objKey, objValue, to, isLast = false }) => {
+  if (typeof objValue === 'object') {
+    const isNestedKeyValue = objValue[0].objKey
+    if (isNestedKeyValue) {
+      return (
+        <>
+          <Entry className="flex">
+            <span className="key">{`"${objKey}"`}</span>
+            <Mark>:</Mark>
+            <Foldable className="punctuation">{'{'}</Foldable>
+          </Entry>
+          <Entry className="level-2">
+            {objValue.map((x, i) => (
+              <KeyValue
+                key={concatString(x.objKey, i)}
+                objKey={x.objKey}
+                objValue={x.objValue}
+                to={x.to}
+                isLast={i === objValue.length - 1}
+              />
+            ))}
+          </Entry>
+          <Entry>
+            <span className="punctuation">{'}'}</span>
+            {!isLast && <Mark>,</Mark>}
+          </Entry>
+        </>
+      )
+    }
+
+    return (
+      <Entry className={className}>
+        <span className="key">{`"${objKey}"`}</span>
+        <Mark>:</Mark>
+        <span className="punctuation">[</span>
+        {objValue.map((x, i) => (
+          <React.Fragment key={concatString(x.objKey, i)}>
+            {typeof x.objValue === 'object' ? (
+              <KeyValue objValue={x.objValue} />
+            ) : (
+              <>
+                {getValueElement(
+                  x.to,
+                  x.objValue,
+                  `${getValueClassName(x.objValue)}${x.className ? ` ${x.className}` : ''}`,
+                )}
+                {i !== objValue.length - 1 && <Mark>,</Mark>}
+              </>
+            )}
+          </React.Fragment>
+        ))}
+        <span className="punctuation">]</span>
+        <Mark>,</Mark>
+      </Entry>
+    )
+  }
+
+  return (
+    <Entry className={className}>
+      <span className="key">{`"${objKey}"`}</span>
+      <Mark>:</Mark>
+      {children || getValueElement(to, objValue, getValueClassName(className))}
+      {!isLast && <Mark>,</Mark>}
+    </Entry>
+  )
+}
+
+export default KeyValue
+
 const Entry = styled.div`
   font-family: ${(p: ThemedProp) => p.theme.fonts.monospace};
   font-size: 1em;
@@ -49,126 +149,18 @@ const Entry = styled.div`
   }
   &.search {
     display: flex;
-      input {
-        flex: 1;
-        background: transparent;
-        border: 0;
-        border-bottom: 1px dashed;
-        &:focus + button {
-          opacity: 1;
-        }
-        ${mediaqueries.phone` width: 100px; `};
+    input {
+      flex: 1;
+      background: transparent;
+      border: 0;
+      border-bottom: 1px dashed;
+      &:focus + button {
+        opacity: 1;
       }
+      ${mediaqueries.phone` width: 100px; `};
     }
   }
   &.flex {
     display: flex;
   }
 `
-
-interface Props {
-  className?: string
-  objKey?: string
-  objValue?: string | number | Props[]
-  to?: string
-  isLast?: boolean
-}
-const concatString = (x: string | number = '', y: string | number) => `${x}${y}`
-
-const KeyValue: React.FC<Props> = ({ className, children, objKey, objValue, to, isLast = false }) => {
-  const valueClassName = `value-${typeof objValue === 'number' ? 'number' : 'string'}`
-
-  if (typeof objValue === 'object') {
-    const isKeyValue = objValue[0].objKey
-    if (isKeyValue) {
-      return (
-        <>
-          <Entry className="flex">
-            <span className="key">{`"${objKey}"`}</span>
-            <Mark>:</Mark>
-            <Foldable className="punctuation">{'{'}</Foldable>
-          </Entry>
-          <Entry className="level-2">
-            {objValue.map((x, i) => (
-              <KeyValue
-                key={concatString(x.objKey, i)}
-                objKey={x.objKey}
-                objValue={x.objValue}
-                to={x.to}
-                isLast={i === objValue.length - 1}
-              />
-            ))}
-          </Entry>
-          <Entry>
-            <span className="punctuation">{'}'}</span>
-            {!isLast && <Mark>,</Mark>}
-          </Entry>
-        </>
-      )
-    }
-
-    return (
-      <Entry className={className}>
-        <span className="key">{`"${objKey}"`}</span>
-        <Mark>:</Mark>
-        <span className="punctuation">[</span>
-        {objValue.map((x, i) => {
-          const elClass = `value-${typeof x.objValue === 'number' ? 'number' : 'string'}${
-            x.className ? ` ${x.className}` : ''
-          }`
-          return (
-            <React.Fragment key={concatString(x.objKey, i)}>
-              {x.to ? (
-                <Link
-                  to={x.to}
-                  className={elClass}
-                  target="_blank"
-                  rel="noopener nofollow"
-                  data-a11y="false"
-                  aria-label={`Link to ${x.to}`}
-                >
-                  {x.objValue}
-                </Link>
-              ) : (
-                <span className={elClass}>{x.objValue}</span>
-              )}
-              {i !== objValue.length - 1 && <Mark>,</Mark>}
-            </React.Fragment>
-          )
-        })}
-        <span className="punctuation">]</span>
-        <Mark>,</Mark>
-      </Entry>
-    )
-  }
-
-  let childElement = children
-
-  if (!children) {
-    if (to) {
-      childElement = /^(http|mail|tel)/.test(to || '') ? (
-        // eslint-disable-next-line react/jsx-no-target-blank
-        <a href={to} target="_blank" rel="noreferrer" className={valueClassName}>
-          {objValue}
-        </a>
-      ) : (
-        <Link to={to} className={valueClassName}>
-          {objValue}
-        </Link>
-      )
-    } else {
-      childElement = <span className={valueClassName}>{objValue}</span>
-    }
-  }
-
-  return (
-    <Entry className={className}>
-      <span className="key">{`"${objKey}"`}</span>
-      <Mark>:</Mark>
-      {childElement}
-      {!isLast && <Mark>,</Mark>}
-    </Entry>
-  )
-}
-
-export default KeyValue
